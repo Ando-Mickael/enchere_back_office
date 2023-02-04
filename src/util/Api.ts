1e40101873aeeaf9b1d43d20345cd82e1429d958
Ando Ramahenina <ando.mickael25@gmail.com>
2023-02-04 10:47:42 +0300

    Update Api.ts

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,3 +1,2 @@
-export const baseUrl = "http://localhost:8080";
-// export const baseUrl = "https://encheres5ws-production.up.railway.app";
-// export const baseUrl = "https://encheres5ws-production-6718.up.railway.app";
+// export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://ando-production.up.railway.app";
